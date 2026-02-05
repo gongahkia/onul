6fcf4be90716f1e744b97b154220c667bb66cc69
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-02-05 15:44:07 +0800

    Setup Vitest for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import { resolve } from 'path'
@@ -5,6 +6,10 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  test: {
+    globals: true,
+    environment: 'node',
+  },
   build: {
     rollupOptions: {
       input: {
